42c5e83a937a894615aae91947dbadecff8249ee
Ivan Ivanov <slayer_word_11@outlook.com>
2015-08-18 10:54:13 +0300

    create Enity Framework Blog System project

---
+++ BlogSystem.ConsoleClient/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace BlogSystem.ConsoleClient
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+
+    using BlogSystem.Data;
+    using BlogSystem.Data.Migrations;
+    using BlogSystem.Data.Repositories;
+    using BlogSystem.Models;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var db = new BlogSystemDbContext();
+
+            db.Users.Add(new User
+          {
+                Username = "csyntax",
+                Gender = Gender.Male,
+                RegistrationDate = DateTime.Now,
+                FullName = "Ivan Ivanov",
+                Birthday = new DateTime(1998, 12, 10),
+                ContactInfo = new UserContactInfo
+               {
+                    Twitter = "@csyntax",
+                    Facebook = "IvanIvanov",
+                    PhoneNumber = "0888888888",
+                    Skype = "SkypeProfile"
+                }
+            });
+
+           db.SaveChanges();
+        }
+    }
+}
