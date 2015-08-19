e241e6c170a76950522b151e82facb64aa59b786
Ivan Ivanov <slayer_word_11@outlook.com>
2015-08-19 21:53:08 +0300

    update all project (models, data, context)
    and update db configuration

---
+++ BlogSystem.ConsoleClient/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace BlogSystem.ConsoleClient
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+
+    using BlogSystem.Data;
+    using BlogSystem.Data.Repositories;
+    using BlogSystem.Models;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //todo db
+            var db = new BlogSystemDbContext();
+
+            db.Users.Add(new User
+           {
+               Username = "csyntax",
+               Gender = Gender.Male,
+               RegistrationDate = DateTime.Now,
+               FullName = "Ivan Ivanov",
+               Birthday = new DateTime(1876, 11, 12),
+               ContactInfo = new UserContactInfo
+               {
+                   Twitter = "@slayer.word.11",
+                    Facebook = "IvanIvanov",
+                    PhoneNumber = "0888888888",
+                  Skype = "ivanivanov982"
+                }
+           });
+
+            db.SaveChanges();
+        }
+    }
+}
